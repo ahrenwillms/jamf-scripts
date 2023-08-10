bf36f35832283a86ac1886301f015ae38c8007da
Ahren Willms <ahren.willms@mac.com>
2023-08-10 17:17:22 -0500

    Added comments

--- install_latest_google_chrome_for_enterprise.sh
+++ install_latest_google_chrome_for_enterprise.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Download PKG file from Google
 curl -o /tmp/GoogleChrome.pkg https://dl.google.com/dl/chrome/mac/universal/stable/gcem/GoogleChrome.pkg
 
+# Install the package
 installer -target / -pkg /tmp/GoogleChrome.pkg
 
+# Remove the PKG file
 rm /tmp/GoogleChrome.pkg
 
 exit 0
--- install_latest_google_drive_for_desktop.sh
+++ install_latest_google_drive_for_desktop.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# Download DMG file from Google
 curl -o /tmp/GoogleDrive.dmg https://dl.google.com/drive-file-stream/GoogleDrive.dmg
 
+# Mount the disk image
 hdiutil mount /tmp/GoogleDrive.dmg
 
+# Install the package
 installer -pkg /Volumes/Install\ Google\ Drive/GoogleDrive.pkg -target /
 
+# Unmount the disk image
 hdiutil unmount /Volumes/Install\ Google\ Drive/
 
+# Remove the DMG file
 rm /tmp/GoogleDrive.dmg
 
 exit 0
